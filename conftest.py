0b3305a013a03840ede8b6486f33f4def306b9a5
Vikas Tatipaka <vikas@mac.home>
2026-04-29 01:49:21 -0400

    Enabled cross-browser testing using Playwright built-in fixtures

--- conftest.py
+++ conftest.py
@@ -3,14 +3,14 @@ from playwright.sync_api import Playwright
 from pytest_playwright.pytest_playwright import context
 
 
-@pytest.fixture(scope="function")
-def page(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    yield page
-    context.close()
-    browser.close()
+# @pytest.fixture(scope="function")
+# def page(playwright: Playwright):
+#     browser = playwright.chromium.launch(headless=False)
+#     context = browser.new_context()
+#     page = context.new_page()
+#     yield page
+#     context.close()
+#     browser.close()
 
 
 
