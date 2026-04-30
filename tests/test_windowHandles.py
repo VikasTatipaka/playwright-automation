dc5d48149c5d6693b37c1e9efb1bf5bf7b01a8ad
Vikas Tatipaka <vikas@mac.home>
2026-04-29 20:18:48 -0400

    adding new tests

--- tests/test_windowHandles.py
+++ tests/test_windowHandles.py
@@ -1,4 +1,5 @@
 import time
+from multiprocessing import process
 
 from playwright.sync_api import Page, expect
 
@@ -33,6 +34,47 @@ def test_frames(page: Page):
     frames = page.frame_locator("#courses-iframe")
 
     expect(frames.locator("body")).to_contain_text("An Academy to")
+
+def test_dynamicTable(page: Page):
+    page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+
+    # identify column: price
+    # identify rows: rice
+    # assert price of rice
+
+    cols = page.locator("th")
+    count = cols.count()
+    print(count)
+    index= -1
+    for i in range(count):
+        if cols.nth(i).inner_text() =="Price":
+            index = i
+            break
+
+    rice_row = page.locator("tr").filter(has_text= "Rice")
+    price = rice_row.locator("td").nth(index).inner_text().strip()
+    print(price)
+
+def test_langtable(page: Page):
+    page.goto("https://practicetestautomation.com/practice-test-table/")
+
+    cols = page.locator("th")
+    count = cols.count()
+    index= -1
+    for i in range(count):
+        if cols.nth(i).inner_text() =="Language":
+            index = i
+            break
+
+    rows = page.locator("//table[@id = 'courses_table']/tbody/tr[not(contains(@style, 'display: none'))]")
+    rowcount = rows.count()
+    for i in range(rowcount):
+        language = rows.nth(i).locator("td").nth(index).inner_text().strip()
+        print(language)
+
+
+
+
 
 
 
