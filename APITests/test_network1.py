8235e6f783306370195fa3bc008d3eb94fac4753
Vikas Tatipaka <vikas@mac.home>
2026-05-01 13:35:23 -0400

    Added Dynamic

--- APITests/test_network1.py
+++ APITests/test_network1.py
@@ -1,6 +1,6 @@
 import time
 
-from playwright.sync_api import Playwright, Page
+from playwright.sync_api import Playwright, Page, expect
 
 
 #https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/69e70531f86ba51a6577f06f
@@ -16,7 +16,9 @@ def test_network1(page:Page):
     page.get_by_placeholder("email@example.com").fill("vickyipad4@icloud.com")
     page.get_by_placeholder("enter your passsword").fill("Vikas@123")
     page.get_by_role("button", name="Login").click()
-    page.get_by_role("button", name="ORDERS").click()
+    orders_btn= page.get_by_role("button", name="ORDERS")
+    expect(orders_btn).to_be_visible()
+    orders_btn.click()
     text = page.locator(".mt-4").text_content()
-    time.sleep(10)
+
     print(text)
